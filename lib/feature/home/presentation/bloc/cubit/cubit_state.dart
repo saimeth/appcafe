eccfcfab7475e296fe8dc132a06fc28bda1c69f8
saimeth <diazsaimeth@gmail.com>
2025-08-16 17:27:12 -0500

    carpetas

---
+++ lib/feature/home/presentation/bloc/cubit/cubit_state.dart
@@ -0,0 +1,10 @@
+part of 'cubit_cubit.dart';
+
+sealed class CubitState extends Equatable {
+  const CubitState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class CubitInitial extends CubitState {}
